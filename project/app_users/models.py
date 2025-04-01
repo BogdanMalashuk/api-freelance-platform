ec4505de91f490d1058ec946f1eea184c14ee027
BogdanMalashuk <kufar.120@gmail.com>
2025-04-01 13:05:22 +0300

    добавил модель User (пользователь)

--- project/app_users/models.py
+++ project/app_users/models.py
@@ -3,10 +3,8 @@ from django.db import models
 
 
 class User(AbstractUser):
-    username = None
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    middle_name = models.CharField(max_length=50, blank=True, null=True)
     nickname = models.CharField(max_length=50, unique=True)
     login = models.CharField(max_length=50, unique=True)
     phone = models.CharField(unique=True, blank=True, null=True)
@@ -14,7 +12,7 @@ class User(AbstractUser):
     balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
     USERNAME_FIELD = "login"
-    REQUIRED_FIELDS = ["first_name", "last_name", "nickname", "phone"]
+    REQUIRED_FIELDS = ["first_name", "last_name", "phone"]
 
     def __str__(self):
         return self.login
