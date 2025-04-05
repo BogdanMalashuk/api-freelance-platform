5c901e78f73609607a69af07e44ffa9c0d5e528a
BogdanMalashuk <kufar.120@gmail.com>
2025-04-05 22:21:57 +0300

    сделал сериализатор UserRegistration создал файл permissions с правами

--- api/apps/app_users/serializers.py
+++ api/apps/app_users/serializers.py
@@ -6,3 +6,22 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'login', 'phone', 'about', 'balance']
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'login', 'password', 'phone', 'about', 'balance']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            login=validated_data['login'],
+            password=validated_data['password'],
+            phone=validated_data.get('phone'),
+            about=validated_data.get('about'),
+            balance=validated_data.get('balance', 0.00)
+        )
+        return user
