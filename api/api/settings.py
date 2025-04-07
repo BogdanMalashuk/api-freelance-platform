0284fa3dc31465f511d43659c711b4eb7fbc23c1
BogdanMalashuk <kufar.120@gmail.com>
2025-04-07 13:58:41 +0300

    сделал тестовые настройки для докера

--- api/api/settings.py
+++ api/api/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4@a&7-^ubgo*bn&svhuc0*0($d$^rgrbt)n0-juw6jzei4sx(3'
-DEBUG = True
+SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 
 ALLOWED_HOSTS = ['*']
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'freelance_db',
-        'USER': 'postgres',
-        'PASSWORD': 'psql_password_pass7',
-        'HOST': 'db',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
